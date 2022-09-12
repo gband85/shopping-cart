ab50fe6cbcf63fd9cd088599a986cbd1aae1b657
gband85 <garrett@gband.net>
2022-09-12 16:27:12 -0500

    add cart page

---
+++ src/Cart.js
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar";
+
+const Cart=()=>{
+return (
+    <div>
+    <Navbar />
+                <div className="mai"></div>
+                </div>
+)
+}
+
+export default Cart;
