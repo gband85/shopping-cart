3b42b020f2a9ec60af576874ac137e40f5173e97
gband85 <garrett@gband.net>
2022-09-12 16:26:47 -0500

    add homepage

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar";
+import './Home.css'
+
+const Home=()=>{
+    return (
+        <div>
+            <Navbar/>
+            <div className="main"></div>
+{/* <img /> */}
+        </div>
+    )
+}
+
+export default Home;
