b9cff7e5819f5498cae635d406fb0358d6cca536
gband85 <garrett@gband.net>
2022-09-12 15:17:04 -0500

    add shop

---
+++ src/Shop.js
@@ -0,0 +1,15 @@
+import Item from "./Item";
+import Navbar from "./Navbar";
+
+const Shop=()=>{
+    return (
+        <div>
+    <Navbar />
+    <div className="main">
+        <Item />
+    </div>    
+    </div>
+    )
+}
+
+export default Shop;
