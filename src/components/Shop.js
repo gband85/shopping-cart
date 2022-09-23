35a94293f4ee2c9c09a325db7d781fd12eae2061
gband85 <garrett@gband.net>
2022-09-23 12:19:04 -0500

    create props manually

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,42 +1,50 @@
 import Item from "./Item";
 import { useState } from 'react';
 import Navbar from "./Navbar";
-import chicken from "../images/chicken.jpg"
+import chicken from '../images/chicken.jpg';
+import groucho from "../images/groucho.jpg";
+import usb_rock from "../images/usb-rock.jpg";
+import useless from "../images/useless.jpg";
+import whoopee from "../images/whoopee.jpg";
 import './Shop.css'
 
 const Shop=()=>{
-    function importAll(images,titles) {
-        const projects = [];
-        
-        images.keys().forEach((image,index) => {
-          //images[item.replace('./', '')] = r(item);
-          projects.push({title:titles[index],image: images(image),href:'/projects/'+images(image).match(/(?<=\/)[a-z-_]*(?=\.)/)})
-        });
-    //    for (let i=0;i<15;i++ ) {
-    // images[i].title=titles[i]
-    //    }
-        return projects;
-      }
-    
-    
-        const titlesArray=[   
-      "Rubber Chicken",
-      "Groucho Glasses",
-      "USB Pet Rock",
-      "Useless Box",
-      "Whoopee Cushion"
-      ];
-    
-      const [projects,setProjects]=useState(importAll(
-        require.context('../images', false, /\.(png|jpg)$/),titlesArray
-      ));
+  const ITEM_DATA=[
+     {
+      itemID: "122863",
+      itemTitle: "Rubber Chicken",
+      itemImage: chicken,
+    },
+    {
+      itemID:  "122864",
+      itemTitle: "Groucho Glasses",
+      itemImage: groucho,
+    },
+    {
+      itemID:  "122865",
+      itemTitle: "USB Pet Rock",
+      itemImage: usb_rock,
+    },
+    {
+      itemID:  "122866",
+      itemTitle: "Useless Box",
+      itemImage: useless,
+    },
+    {
+      itemID:  "122867",
+      itemTitle: "Whoopee Cushion",
+      itemImage: whoopee,
+    },
+  ]
+  const [items,setItems]=useState(ITEM_DATA)
+console.log(items)
     return (
         <div>
     <Navbar />
     <div className="mai">
     <div className="items">
-    {projects.map(project=>{
-return  <Item title={project.title} image={project.image}/>
+    {items.map(item=>{
+return  <Item id={item.itemID} title={item.itemTitle} image={item.itemImage} />
     })}
        
         </div>
