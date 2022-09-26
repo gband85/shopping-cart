58dc6b87915a5ac837ebcb7aa6d1095eea8f072e
gband85 <garrett@gband.net>
2022-09-25 21:20:23 -0500

    add different results based on prop

--- src/components/Item.js
+++ src/components/Item.js
@@ -17,22 +17,40 @@ itemImage: props.itemImage,
   const handleChange=(e)=>{
 setItem({...item,itemQuantity: e.target.value,})
   }
-    return (
-        <div class="card">
-        <a href={props.href}>
-            <img src={props.itemImage} class="card__img" alt="" id='itemImage'/>
-        
-          {/* <div class="card__overlay"> */}
-            <h3 class="card__title" id='itemTitle'>{props.itemTitle}</h3>
-            <form onSubmit={handleSubmit}>
-            <input type="number" min="0" max="10" value={item.itemQuantity} onChange={handleChange} id="itemQuantity"/>
-            <button type="submit">Add to Cart</button>
-            </form>
+  
+  if (props.displayItem) {
+  return (
+    <div class="card">
+    <a href={props.href}>
+        <img src={props.itemImage} class="card__img" alt="" id='itemImage'/>
+    
+      {/* <div class="card__overlay"> */}
+        <h3 class="card__title" id='itemTitle'>{props.itemTitle}</h3>
+        <form onSubmit={handleSubmit}>
+        <input type="number" min="0" max="10" value={item.itemQuantity} onChange={handleChange} id="itemQuantity"/>
+        <button type="submit">Add to Cart</button>
+        </form>
 
-          {/* </div> */}
-        </a>
-      </div>
-    )
+      {/* </div> */}
+    </a>
+  </div>
+)
+  }
+  else {
+    return (
+      <div class="card">
+      <a href={props.href}>
+          <img src={props.itemImage} class="card__img" alt="" id='itemImage'/>
+      
+        {/* <div class="card__overlay"> */}
+          <h3 class="card__title" id='itemTitle'>{props.itemTitle}</h3>
+  <button onClick={()=>props.deleteItem(props.itemID)}>delete</button>
+        {/* </div> */}
+      </a>
+    </div>
+  )
+  }
+
 }
 
 export default Item;
