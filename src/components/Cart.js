722c3b659d2b6c4f3d34530070fc7062c46c612f
gband85 <garrett@gband.net>
2022-09-27 18:39:11 -0500

    Add quantity

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,13 +1,13 @@
 import Item from "./Item";
 import Navbar from "./Navbar";
 import "./Cart.css"
+import { useState,useEffect } from "react";
 
 const Cart=(props)=>{
-    
 return (
     <div className="cart">
-{props.cartItems.map(item=>{
-return <Item itemID={item.itemID} itemTitle={item.itemTitle} itemImage={item.itemImage} deleteItem={props.deleteItem}/>
+{props.cart.map(item=>{
+return <Item itemID={item.itemID} itemTitle={item.itemTitle} itemImage={item.itemImage} deleteItem={props.deleteItem} itemQuantity={item.itemQuantity}/>
 })}
                 </div>
 )
