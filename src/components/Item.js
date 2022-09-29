44bc2b846e1c3f44d1e29346fc08d405e974a323
gband85 <garrett@gband.net>
2022-09-29 15:03:38 -0500

    add increment/decrement buttons

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,57 +1,83 @@
-import { useState } from 'react';
-import './Item.css'
+import { useState } from "react";
+import "./Item.css";
 
-const Item=(props)=>{
-  const [item,setItem]=useState({
+const Item = (props) => {
+  const [item, setItem] = useState({
     itemID: props.itemID,
-    itemQuantity: "",
-itemTitle: props.itemTitle,
-itemImage: props.itemImage,
-
-  })
-  const handleSubmit=(e)=>{
+    itemQuantity: 0,
+    itemTitle: props.itemTitle,
+    itemImage: props.itemImage,
+    itemPrice: props.itemPrice,
+  });
+  const handleSubmit = (e) => {
     e.preventDefault();
     // console.log(item)
-    props.addItem(item)
-  }
-  const handleChange=(e)=>{
-setItem({...item,itemQuantity: parseInt( e.target.value),})
-  }
-  
-  if (props.displayItem) {
-  return (
-    <div className="card">
-    <a href={props.href}>
-        <img src={props.itemImage} className="card__img" alt="" id='itemImage'/>
-    
-      {/* <div class="card__overlay"> */}
-        <h3 className="card__title" id='itemTitle'>{props.itemTitle}</h3>
-        <form onSubmit={handleSubmit}>
-        <input type="number" min="0" max="10" value={item.itemQuantity} onChange={handleChange} id="itemQuantity"/>
-        <button type="submit">Add to Cart</button>
-        </form>
+    setItem({...item, itemQuantity: 0})
+    props.addItem(item);
+  };
+  const handleChange = (e) => {
+    setItem({ ...item, itemQuantity: parseInt(e.target.value) });
+  };
+  const decreaseQuantity = ()=>setItem({ ...item, itemQuantity: item.itemQuantity-1});
 
-      {/* </div> */}
-    </a>
-  </div>
-)
-  }
-  else {
+  const increaseQuantity = ()=>setItem({ ...item, itemQuantity: item.itemQuantity+1});
+
+  if (props.displayItem) {
     return (
       <div className="card">
-      <a href={props.href}>
-          <img src={props.itemImage} className="card__img" alt="" id='itemImage'/>
-      
-        {/* <div class="card__overlay"> */}
-          <h3 className="card__title" id='itemTitle'>{props.itemTitle}</h3>
-          <h3>Quantity: {props.itemQuantity}</h3>
-  <button onClick={()=>props.deleteItem(props.itemID)}>delete</button>
-        {/* </div> */}
-      </a>
-    </div>
-  )
-  }
+        <a href={props.href}>
+          <img
+            src={props.itemImage}
+            className="card__img"
+            alt=""
+            id="itemImage"
+          />
 
-}
+          {/* <div class="card__overlay"> */}
+          <h3 className="card__title" id="itemTitle">
+            {props.itemTitle}
+          </h3>
+          <h3>${props.itemPrice}</h3>
+          <form onSubmit={handleSubmit}>
+          <div className="number-input">
+         <button type="button" onClick={decreaseQuantity}>-</button>  
+         <input
+              type="number"
+              value={item.itemQuantity}
+              onChange={handleChange}
+              id="itemQuantity"
+            /><button type="button" onClick={increaseQuantity}>+</button>
+            </div>
+            <button type="submit">Add to Cart</button>
+          </form>
+
+          {/* </div> */}
+        </a>
+      </div>
+    );
+  } else {
+    return (
+      <div className="card">
+        <a href={props.href}>
+          <img
+            src={props.itemImage}
+            className="card__img"
+            alt=""
+            id="itemImage"
+          />
+
+          {/* <div class="card__overlay"> */}
+          <h3 className="card__title" id="itemTitle">
+            {props.itemTitle}
+          </h3>
+          <h3>${props.itemPrice}</h3>
+          <h3>Quantity: {props.itemQuantity}</h3>
+          <button onClick={() => props.deleteItem(props.itemID)}>delete</button>
+          {/* </div> */}
+        </a>
+      </div>
+    );
+  }
+};
 
 export default Item;
