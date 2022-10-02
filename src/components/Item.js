37868dbe20cb779b57129d5f910c9e1018f679dd
gband85 <garrett@gband.net>
2022-10-02 14:31:59 -0500

    set state whether item is display or cart

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,13 +2,30 @@ import { useState } from "react";
 import "./Item.css";
 
 const Item = (props) => {
-  const [item, setItem] = useState({
+  const [item, setItem] = useState(()=>{
+    if (props.displayItem) {
+      return (
+    {
     itemID: props.itemID,
-    itemQuantity: 0,
+    itemQuantity: 1,
     itemTitle: props.itemTitle,
     itemImage: props.itemImage,
     itemPrice: props.itemPrice,
-  });
+  }
+      )
+}
+else {
+  return (
+    {
+    itemID: props.itemID,
+    itemQuantity: props.itemQuantity,
+    itemTitle: props.itemTitle,
+    itemImage: props.itemImage,
+    itemPrice: props.itemPrice,
+  }
+      )
+}
+});
   const handleSubmit = (e) => {
     e.preventDefault();
     // console.log(item)
