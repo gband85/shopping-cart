df96cb54bd043a0c6fcc3d966c7eda891099d662
gband85 <garrett@gband.net>
2022-10-02 14:37:26 -0500

    hide cart page and display cart quantity if cart isn't empty

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,43 +1,78 @@
 import Item from "./Item";
 import Navbar from "./Navbar";
-import "./Cart.css"
-import { useState,useEffect } from "react";
+import "./Cart.css";
+import { useState, useEffect } from "react";
 
-const Cart=(props)=>{
-const deleteItem=(id)=>{
-    console.log("cart!")
+const Cart = (props) => {
+  const deleteItem = (id) => {
+    console.log("cart!");
     props.deleteItem(id);
-}
+  };
 
-    if (props.cartPage) {
-        return (
-            <div>
-            <Navbar/>
-            <div className="cart">
-        {props.count>0 ? <h3># of items: {props.count }</h3> : <h3>Your Cart is empty!</h3>}
-        <h3>{props.total}</h3>
-        <div className="items">
-        {props.cart.map(item=>{
-        return <Item itemID={item.itemID} itemTitle={item.itemTitle} itemImage={item.itemImage} deleteItem={deleteItem} itemQuantity={item.itemQuantity} itemPrice={item.itemPrice}/>
-        })}
-</div>
-                        </div>
-                </div>        
-        )
+  // if (props.cartPage) {
+  //   return (
+  //     <div>
+  //       <Navbar count={props.count}/>
+  //       <div className="cart">
+  //         {props.count > 0 ? (
+  //           <h3># of items: {props.count}</h3>
+  //         ) : (
+  //           <h3>Your Cart is empty!</h3>
+  //         )}
+  //         <h3>Total: ${props.total}</h3>
+  //         <div className="cart__items">
+  //           {props.cart.map((item) => {
+  //             return (
+  //               <Item
+  //               addItem={props.addToCart}
+  //                 itemID={item.itemID}
+  //                 itemTitle={item.itemTitle}
+  //                 itemImage={item.itemImage}
+  //                 deleteItem={deleteItem}
+  //                 itemQuantity={item.itemQuantity}
+  //                 itemPrice={item.itemPrice}
+  //               />
+  //             );
+  //           })}
+  //         </div>
+  //       </div>
+  //     </div>
+  //   );
+  // } 
+  // else {
+    let visible;
+    if (props.cartVisibility) {
+visible = "cart show";
     }
-else {
+    else {
+      visible="cart hide";
+    }
     return (
-        <div className="cart">
-   {props.count>0 ? <h3># of items: {props.count}</h3> : null}
-   <h3>{props.total}</h3>
-   <div className="items">
-    {props.cart.map(item=>{
-    return <Item itemID={item.itemID} itemTitle={item.itemTitle} itemImage={item.itemImage} deleteItem={deleteItem} itemQuantity={item.itemQuantity} itemPrice={item.itemPrice}/>
-    })}
-    </div>
-                    </div>
-    )
-}
-}
+      <div className={visible}>
+                  {props.count > 0 ? (
+            <h3># of items: {props.count}</h3>
+          ) : (
+            <h3>Your Cart is empty!</h3>
+          )}
+        <h3>Total: ${props.total}</h3>
+        <div className="cart__items">
+          {props.cart.map((item) => {
+            return (
+              <Item
+              addItem={props.addItem}
+                itemID={item.itemID}
+                itemTitle={item.itemTitle}
+                itemImage={item.itemImage}
+                deleteItem={deleteItem}
+                itemQuantity={item.itemQuantity}
+                itemPrice={item.itemPrice}
+              />
+            );
+          })}
+        </div>
+      </div>
+    );
+  // }
+};
 
 export default Cart;
