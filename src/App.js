ce7630a5004f0819f1c5ea4108a05a0f8d7a55b3
gband85 <garrett@gband.net>
2022-10-05 19:06:04 -0500

    remove cart functions

--- src/App.js
+++ src/App.js
@@ -7,86 +7,13 @@ import Cart from "./components/Cart";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [cart, setCart] = useState(function () {
-    if (window.localStorage.getItem("cart") !== null)
-      return JSON.parse(window.localStorage.getItem("cart"));
-    else return [];
-  });
-  useEffect(() => {
-    const data = window.localStorage.getItem("cart");
-    if (data !== null) setCart(JSON.parse(data));
-  }, []);
-
-  useEffect(() => {
-    window.localStorage.setItem("cart", JSON.stringify(cart));
-  }, [cart]);
-
-  const addToCart = (itemToAdd) => {
-    let newCart;
-    const exist = cart.some(function (cartItem) {
-      return cartItem.itemID == itemToAdd.itemID;
-    });
-    // console.log(exist)
-    if (exist) {
-      newCart = cart.map(function (cartItem) {
-        if (cartItem.itemID == itemToAdd.itemID) {
-          return {
-            ...cartItem,
-            itemQuantity: cartItem.itemQuantity + itemToAdd.itemQuantity,
-          };
-          //  cartItem;
-          // console.log(cartItem)
-        }
-        return cartItem;
-      });
-      console.log(newCart);
-    } else {
-      newCart = [...cart, itemToAdd];
-    }
-
-    setCart(newCart);
-  };
-  const deleteItem = (id) => {
-    const remainingItems = cart.filter((item) => id !== item.itemID);
-    console.log(remainingItems);
-    setCart(remainingItems);
-  };
-
-  //  }, [cart])
-  // useEffect(()=>{
-  //   setCart(JSON.parse(window.localStorage.getItem('cart')));
-  // },[])
-
-  // console.log(cartItems)
-
+  
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route
-          path="/shop"
-          element={
-            <Shop
-              cart={cart}
-              addToCart={addToCart}
-              deleteItem={deleteItem}
-              count={
-                cart.length > 0
-                  ? cart.reduce((prev, curr) => prev + curr.itemQuantity, 0)
-                  : 0
-              }
-              total={
-                cart.length > 0
-                  ? cart.reduce(
-                      (prev, curr) => prev + curr.itemPrice * curr.itemQuantity,
-                      0
-                    )
-                  : 0
-              }
-            />
-          }
-        />
-        <Route
+        <Route path="/" element={<Home />}/>
+        <Route path="/shop" element={<Shop />}/>
+        {/* <Route
           path="/cart"
           element={
             <Cart
@@ -109,7 +36,7 @@ function App() {
               }
             />
           }
-        />
+        /> */}
       </Routes>
     </BrowserRouter>
   );
