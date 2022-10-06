410f1ac440f885d0300a63271f4dc5e2c41f13be
gband85 <garrett@gband.net>
2022-10-05 19:01:56 -0500

    split Item component into separate components for cart and shop

---
+++ src/components/CartItem.js
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import "./Item.css";
+
+const CartItem = (props) => {
+  const [item, setItem] = useState(  
+    {
+    itemID: props.itemID,
+    itemQuantity: props.itemQuantity,
+    itemTitle: props.itemTitle,
+    itemImage: props.itemImage,
+    itemPrice: props.itemPrice,
+  }
+);
+
+  const handleChange = (e) => {
+    setItem({ ...item, itemQuantity: parseInt(e.target.value) });
+  };
+  const decreaseQuantity = () => {
+    if (item.itemQuantity === 1) {
+      return;
+    }
+    setItem({ ...item, itemQuantity: item.itemQuantity - 1 });
+    
+      props.addItem({...item,itemQuantity: -1})
+    
+  };
+  const increaseQuantity = () => {
+    setItem({ ...item, itemQuantity: item.itemQuantity + 1 });
+    
+      props.addItem({...item,itemQuantity: 1})
+    
+  }
+  
+  
+    return (
+      <div className="card card--cart">
+
+          <img
+            src={props.itemImage}
+            className="card__img card__img--small"
+            alt=""
+            id="itemImage"
+          />
+
+          {/* <div class="card__overlay"> */}
+          <div className="card__body">
+          <div>
+          <h3 className="card__title" id="itemTitle">
+            {props.itemTitle}
+          </h3>
+          <h3>${props.itemPrice}</h3>
+          </div>
+          <div className="number-input">
+              <button className="number-input__button" type="button" onClick={decreaseQuantity}>
+                -
+              </button>
+              <input
+                type="number"
+                value={props.itemQuantity}
+                onChange={handleChange}
+                id="itemQuantity"
+                className="number-input__field number-input__field--small"
+              />
+              <button className="number-input__button" type="button" onClick={increaseQuantity}>
+                +
+              </button>
+            </div>
+          <button onClick={() => props.deleteItem(props.itemID)}>delete</button>
+          {/* </div> */}
+        </div>
+      </div>
+    );
+  
+};
+
+export default CartItem;
