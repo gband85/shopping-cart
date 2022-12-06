8d61316a0b5c91a3e38cb8c2bfa7eac6ba6554cf
gband85 <garrett@gband.net>
2022-12-05 18:24:08 -0600

    move Cart, visibility and overlay functions to root

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,17 +1,49 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Cart from "./Cart";
 import Navbar from "./Navbar"
 
-const Root=()=>{
+const Root=(props)=>{
+    const [cartVisibility, setCartVisibility]=useState(false);
+
+    const toggleCart=()=>{
+      console.log("go!")
+      setCartVisibility(!cartVisibility)
+  
+    }
+  let overlay;
+  if (cartVisibility) {
+    overlay="overlay open";
+  }
+  else {
+    overlay="overlay";
+  }
     return (
     <>
-    <Navbar/>
+    <Navbar toggleCart={toggleCart} count={props.count}/>
           <div className="container">
-      {/* <div className={overlay}></div> */}
-      <div className="main">
+      <div className={overlay}></div>
         <Outlet/>
-      </div>
-      {/* <Cart/> */}
+        <Cart
+        addItem={props.addToCart}
+          cart={props.cart}
+          deleteItem={props.deleteItem}
+          count={
+                props.cart.length > 0
+                  ? props.cart.reduce((prev, curr) => prev + curr.itemQuantity, 0)
+                  : 0
+              }
+              total={
+                props.cart.length > 0
+                  ? props.cart.reduce(
+                      (prev, curr) => prev + curr.itemPrice * curr.itemQuantity,
+                      0
+                    )
+                  : 0
+              }
+          cartVisibility={cartVisibility}
+          toggleCart={toggleCart}
+        /> 
       </div>
       </>
       )
